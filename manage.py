614d22324e0a02ac88d4f43fa000e2e830174717
lagvilava2811 <your-email@example.com>
2026-07-22 00:44:41 +0300

    Revert "Add Render production deployment blueprint"
    This reverts commit 05b8df2595da664552889d05d6dbdefc1eed2185.

--- manage.py
+++
@@ -1,6 +0,0 @@
-import os,sys
-def main():
- os.environ.setdefault("DJANGO_SETTINGS_MODULE","musea.settings")
- from django.core.management import execute_from_command_line
- execute_from_command_line(sys.argv)
-if __name__=="__main__": main()
